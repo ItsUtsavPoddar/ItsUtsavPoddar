9952f9edb5af32f93329c6ea3062c9d2dc12fe9c
Utsav Poddar <utsavpoddar99@gmail.com>
2024-05-06 03:48:39 +0530

    1.4.1

--- src/About.jsx
+++ src/About.jsx
@@ -43,6 +43,7 @@ const About = () => {
                 </a>
                 <a
                   className="text-gray-500 hover:text-gray-900 dark:hover:text-gray-100"
+                  download
                   href="/utsav_poddar_resume.pdf"
                   target="_blank"
                 >
